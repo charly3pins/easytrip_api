cd41b05ec4ae4d57a229b90d14756e50c376db31
charly3p <charlyfb90@gmail.com>
2016-08-24 19:34:09 +0200

    Angular app

--- WebContent/WEB-INF/app/flights/flights.controller.js
+++ WebContent/WEB-INF/app/flights/flights.controller.js
@@ -9,7 +9,7 @@ angular
 			$http.get('http://localhost:8080/easytripAPI/rest/flights/BCN/ROM')
 			.success(function(data){
 				vm.flights = data;
-				console.log("Results " + vm.flights);
+				console.log("Results: " + vm.flights);
 	    	})
 	    	.error(function(data, status, headers, config) {
       			console.log("AJAX failed to get data, status=" + status);
