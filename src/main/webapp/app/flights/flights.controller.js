29476b645fd65f3d526592312448c52211d37871
charly3p <charlyfb90@gmail.com>
2016-09-25 17:14:34 +0200

    Location Autosuggest in flights view

--- src/main/webapp/app/flights/flights.controller.js
+++ src/main/webapp/app/flights/flights.controller.js
@@ -4,8 +4,8 @@ angular
 		var vm = this;
 
 		vm.formData = {};
-		vm.formData = { adults : "1",
-					apiKey : "prtl6749387986743898559646983194",
+		vm.formData = {
+					adults : "1",
 					children : "0",
 					country : "ES",
 					currency : "EUR",
@@ -30,6 +30,50 @@ angular
 			res.error(function(data, status, headers, config) {
 				console.log("AJAX failed to get data, status=" + status);
       		});
+		}
+		
+		// LOCATION AUTOSUGGEST ORIGEN
+		vm.selectedItemOrigin = [];
+		vm.searchTextChangeOrigin = function () {
+			if(vm.searchTextOrigin.length > 2){ // a partir de 2 carácteres buscamos en la API
+				return $http.get("http://localhost:8080/easytripAPI/rest/suggestLocation/"+vm.searchTextOrigin)
+					.then(function(response){
+						return response.data;
+					}, 
+					function(response){
+						// something went wrong
+						return $q.reject(response.data);
+					});
+			}
+		}
+		
+		// cuando cambia el valor del autocomplete, entramos aquí para hacer el binding con la property de nuestro formData
+		vm.selectedItemChangeOrigin = function () {
+		   if(vm.selectedItemOrigin != null){
+			   vm.formData.originPlace = vm.selectedItemOrigin.PlaceId;
+		   }
+		}
+		
+		// LOCATION AUTOSUGGEST DESTINO
+		vm.selectedItemDest = [];
+		vm.searchTextChangeDest = function () {
+			if(vm.searchTextDest.length > 2){ // a partir de 2 carácteres buscamos en la API
+				return $http.get("http://localhost:8080/easytripAPI/rest/suggestLocation/"+vm.searchTextDest)
+					.then(function(response){
+						return response.data;
+					}, 
+					function(response){
+						// something went wrong
+						return $q.reject(response.data);
+					});
+			}
+		}
+		
+		// cuando cambia el valor del autocomplete, entramos aquí para hacer el binding con la property de nuestro formData
+		vm.selectedItemChangeDest = function () {
+		   if(vm.selectedItemDest != null){
+			   vm.formData.destPlace = vm.selectedItemDest.PlaceId;
+		   }
 		}
 		
 	}]);
