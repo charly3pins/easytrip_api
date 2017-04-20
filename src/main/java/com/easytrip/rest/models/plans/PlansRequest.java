40ecd828d947abcf7a347a3a80ba29d1a461794c
Charly <charlyfb90@gmail.com>
2017-04-20 22:51:48 +0200

    Revert "Plans foursquare"

--- src/main/java/com/easytrip/rest/models/plans/PlansRequest.java
+++ src/main/java/com/easytrip/rest/models/plans/PlansRequest.java
@@ -10,27 +10,15 @@ public class PlansRequest {
 	@JsonIgnoreProperties(ignoreUnknown = true)
 	public static String REDIRECT_CALLBACK = "Callback URL";
 	
-	@JsonIgnoreProperties(ignoreUnknown = true)
-	public static String OAuth = "1PGZEAUTMYNUQ4HTUQOKKDBDCTQG1OQEJCBEOV1JMKGOSHYL";
-	@JsonIgnoreProperties(ignoreUnknown = true)
-	public static String BASE_URL = "https://api.foursquare.com/v2/";
-	
-	String method;
-	
-	String coordenadas;
+	String latitudeLongitude;
 	String near;
 	String placeDate;
-	Integer limit;
-	String offset;
 	
-	//explore
-	String section;
-	
-	public String getCoordenadas() {
-		return coordenadas;
+	public String getLatitudeLongitude() {
+		return latitudeLongitude;
 	}
-	public void setCoordenadas(String coordenadas) {
-		this.coordenadas = coordenadas;
+	public void setLatitudeLongitude(String latitudeLongitude) {
+		this.latitudeLongitude = latitudeLongitude;
 	}
 	public String getNear() {
 		return near;
@@ -43,29 +31,5 @@ public class PlansRequest {
 	}
 	public void setPlaceDate(String placeDate) {
 		this.placeDate = placeDate;
-	}
-	public String getSection() {
-		return section;
-	}
-	public void setSection(String section) {
-		this.section = section;
-	}
-	public String getMethod() {
-		return method;
-	}
-	public void setMethod(String method) {
-		this.method = method;
-	}
-	public Integer getLimit() {
-		return limit;
-	}
-	public String getOffset() {
-		return offset;
-	}
-	public void setLimit(Integer limit) {
-		this.limit = limit;
-	}
-	public void setOffset(String offset) {
-		this.offset = offset;
 	} 
 }
--- src/main/webapp/app/plans/plans.controller.js
+++ src/main/webapp/app/plans/plans.controller.js
@@ -5,92 +5,20 @@ angular
 
 		vm.formData = {};
 		vm.formData = {
-				//latitudeLongitude: "41.396985, 2.148277",
-				near : vm.formData.near,
-				limit: vm.formData.limit,
-				coordenadas : vm.formData.coordenadas,
-				section : "sigths",
-				categories : vm.searchCategory,
+				latitudeLongitude: "41.396985, 2.148277",
+				near : "Barcelona",
 				placeDate : new Date()};
-		vm.formData.near = 'Barcelona';
-		vm.formData.limit = 10;
-		vm.formData.coordenadas = "41.396985, 2.148277";
-		vm.items = [];
-		vm.item = {};
-		vm.item.detalle = {};
 		
- 	    vm.resultSearchVenues = [];
- 	    vm.searchVenue = {};
- 	    vm.searchVenue.detalle = {};
- 	    
- 	    vm.searchCategories = {} ;
- 	    
- 	    vm.clickOnCheckbox = function() {
- 	    	vm.searchCategories = vm.searchCategory;
- 	    }
- 	    
-		vm.explore = function(){
- 	    vm.items.detalle = [];
- 	    
-	        var res = $http.post('http://localhost:8080/easytripAPI/rest/plans/explore', vm.formData);
-			
-			res.success(function(data, status, headers, config) {
-				vm.items = data;
-			});
-
-			res.error(function(data, status, headers, config) {
-				console.log("AJAX failed to get data, status=" + status);
-      		});
-		}
-		
-		vm.search = function(){
-	 	    vm.items.detalle = [];
-		        var res = $http.post('http://localhost:8080/easytripAPI/rest/plans/search', vm.formData);
-				
-				res.success(function(data, status, headers, config) {
-					vm.resultSearchVenues = data;
-				});
-
-				res.error(function(data, status, headers, config) {
-					console.log("AJAX failed to get data, status=" + status);
-	      		});
-			}
-		
-		vm.venueByIdSearch = function(resultSearchVenue){
+		vm.submit = function(){
  	        
-			var index = vm.resultSearchVenues.indexOf(resultSearchVenue);
-			var id = resultSearchVenue.id;
-			
-	        var res = $http.post('http://localhost:8080/easytripAPI/rest/plans/venueById', id);
+	        var res = $http.post('http://localhost:8080/easytripAPI/rest/plans', vm.formData);
 			
 			res.success(function(data, status, headers, config) {
-				vm.resultSearchVenues[index].detalle = data;
+				vm.plansData = data;
 			});
 
 			res.error(function(data, status, headers, config) {
 				console.log("AJAX failed to get data, status=" + status);
       		});
 		}
-		
-		vm.venueById = function(item){
- 	        
-			var index = vm.items.indexOf(item);
-			var id = item.venue.id;
-			
-	        var res = $http.post('http://localhost:8080/easytripAPI/rest/plans/venueById', id);
-			
-			res.success(function(data, status, headers, config) {
-				vm.items[index].detalle = data;
-			});
-
-			res.error(function(data, status, headers, config) {
-				console.log("AJAX failed to get data, status=" + status);
-      		});
-		}
-		
-			vm.color = {};
-	      vm.colors = {Blue: true, Orange: true};
-
-		  
-		               
 	}]);
